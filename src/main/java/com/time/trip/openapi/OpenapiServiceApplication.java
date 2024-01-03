a6d10cb8d4f59a93747acdc2235e556ed0fc2959
changlezhong <changlezhong@foxmain.com>
2024-01-04 07:19:58 +0800

    init project

---
+++ src/main/java/com/time/trip/openapi/OpenapiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.time.trip.openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/time/trip/openapi/OpenapiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time.trip.openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
